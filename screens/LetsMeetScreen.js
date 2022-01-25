cf4e9dd23023f7fcc47936f4ce6aef9f94c79a94
gups <91670399+gsinghg19@users.noreply.github.com>
2022-01-25 14:56:47 +0000

    initial commit of folder structure setup for future coding

---
+++ screens/LetsMeetScreen.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/core";
+import React, { useEffect, useState } from "react";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+const LetsMeetScreen = () => {
+  return (
+    <View>
+      <Text>meeting_screen_placeholder</Text>
+    </View>
+  );
+};
+
+export default LetsMeetScreen;
+
+const styles = StyleSheet.create({});
